bb91da31ad654ff49bd612c7dc0dbecc88adf08d
pkindalov <panayotkindalov@gmail.com>
2017-01-20 01:42:04 +0200

    Created new class IOManager.java and added few simple methods

--- Lab/src/IOManager.java
+++ Lab/src/IOManager.java
@@ -1,7 +1,45 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.LinkedList;
 
 /**
  * Created by r3v3nan7 on 20.01.17.
  */
 public class IOManager {
+
+    public static class OutputWriter{
+
+        public static void writeMessage(String message)
+        {
+            System.out.print(message);
+        }
+
+        public static void writeMessageOnNewLine(String message)
+        {
+            System.out.println(message);
+        }
+
+
+        public static void writeEmptyLine()
+        {
+            System.out.println();
+        }
+
+
+        public static void displayException(String message)
+        {
+            System.out.print(message);
+        }
+
+
+        public static void traverseDirectory(String path){
+            LinkedList<File> subFolders = new LinkedList<File>();
+            File root = new File(path);
+
+            subFolders.add(root);
+        }
+
+
+    }
+
+
 }
