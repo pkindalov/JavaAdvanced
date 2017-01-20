b0a0c32eab5badc355a2cafc8fdcbbceb3ba4b48
pkindalov <panayotkindalov@gmail.com>
2017-01-21 00:32:32 +0200

    Added exercise Lottery

---
+++ Java Syntax/Lottery.java
@@ -0,0 +1,38 @@
+package JavaSyntax;
+
+/**
+ * Created by r3v3nan7 on 17.01.17.
+ */
+public class Lottery {
+
+    public static void main(String[] args){
+
+//        for (int n1 = 1; n1 < 44 ; n1++) {
+//            for (int n2 = n1 + 1; n2 < 45 ; n2++) {
+//                for (int n3 = n2 + 1; n3 < 46 ; n3++) {
+//                    for (int n4 = n3 + 1; n4 < 47 ; n4++) {
+//                        for (int n5 = n4 + 1; n5 < 48 ; n5++) {
+//                            for (int n6 = n5 + 1; n6 < 49 ; n6++) {
+//                                System.out.printf("%d %d %d %d %d %d\n", n1, n2, n3, n4, n5, n6);
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+//
+        for (int n1 = 1; n1 < 11 ; n1++) {
+            for (int n2 = n1 + 1; n2 < 11 ; n2++) {
+                for (int n3 = n2 + 1; n3 < 11 ; n3++) {
+                    System.out.printf("%d %d %d %n", n1, n2, n3);
+                }
+            }
+        }
+
+
+
+
+    }
+
+}
