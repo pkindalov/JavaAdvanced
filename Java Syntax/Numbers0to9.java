936f34fbd8258346e52704d8ad323ec3003e1245
pkindalov <panayotkindalov@gmail.com>
2017-01-21 00:33:03 +0200

    Added exercise Numbers 0 to 9

---
+++ Java Syntax/Numbers0to9.java
@@ -0,0 +1,17 @@
+package JavaSyntax;
+
+/**
+ * Created by r3v3nan7 on 17.01.17.
+ */
+public class Numbers0to9 {
+
+    public static void main(String[] args){
+        int counter = 0;
+
+        while (counter < 10){
+            System.out.println("Number: " + counter);
+            counter++;
+        }
+    }
+
+}
