ff47864d7f19e1c42048e57b1b6ecdc64d2a9840
pkindalov <panayotkindalov@gmail.com>
2017-01-23 12:58:33 +0200

    Fixed error in exercise Formatting numbers

--- Java Syntax/FormattingNumbers.java
+++ Java Syntax/FormattingNumbers.java
@@ -1,5 +1,6 @@
 package JavaSyntax;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.Scanner;
 
 /**
@@ -10,9 +11,22 @@ public class FormattingNumbers {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        int a = Integer.parseInt(scanner.nextLine());
-        double b = Double.parseDouble(scanner.nextLine());
-        double c = Double.parseDouble(scanner.nextLine());
+        String[] nums = scanner.nextLine().split("\\s+");
+
+        int a = Integer.parseInt(nums[0]);
+        double b = Double.parseDouble(nums[1]);
+        double c = Double.parseDouble(nums[2]);
+
+
+        String hex = Integer.toHexString(a).toUpperCase();
+        String binary = String.format("%10s", Integer.toBinaryString(a)).replace(' ', '0');
+
+
+        System.out.printf("|%-10s", hex);
+        System.out.printf("|%s", binary);
+        System.out.printf("|%10.2f", b);
+        System.out.printf("|%-10.3f|", c);
+
 
 
     }
