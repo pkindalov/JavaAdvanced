46a8e83fa3df0bb803533b5021d10b929861137e
pkindalov <panayotkindalov@gmail.com>
2017-01-23 12:54:57 +0200

    Added exercise Formatting Numbers

---
+++ Java Syntax/FormattingNumbers.java
@@ -0,0 +1,20 @@
+package JavaSyntax;
+
+import java.util.Scanner;
+
+/**
+ * Created by r3v3nan7 on 17.01.17.
+ */
+public class FormattingNumbers {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        double b = Double.parseDouble(scanner.nextLine());
+        double c = Double.parseDouble(scanner.nextLine());
+
+
+    }
+
+}
